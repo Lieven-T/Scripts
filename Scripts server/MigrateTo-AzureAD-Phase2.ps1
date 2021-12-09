2eb1acbc7212f4fecb704322edf07496a8923019
Lieven Tronckoe <lieven.tronckoe@romerocollege.be>
2021-12-09 11:17:28 +0100

    Opkuis deel 2

--- Scripts server/MigrateTo-AzureAD-Phase2.ps1
+++ Scripts server/MigrateTo-AzureAD-Phase2.ps1
@@ -1,10 +1,5 @@
 ﻿Start-Transcript c:\temp\migrateto-azuread-phase2.log
 Unregister-ScheduledTask -TaskName "MigrateTo-AzureAD-Phase2" -Confirm:$false
 
-<#$Action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-ExecutionPolicy Bypass C:\Temp\MigrateTo-AzureAD-Phase3.ps1"
-$Trigger = New-ScheduledTaskTrigger -AtStartup
-$Principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-Register-ScheduledTask -TaskName "MigrateTo-AzureAD-Phase3" -TaskPath "\ORC" -Action $Action -Trigger $Trigger -Principal $Principal#>
-
 Install-ProvisioningPackage -PackagePath c:\temp\desktop.ppkg -ForceInstall -QuietInstall
 Stop-Transcript
