f85b0350449dd82cb0ad8cba833dbc1ede952c52
Lieven Tronckoe <lieven.tronckoe@romerocollege.be>
2021-12-09 11:17:39 +0100

    COmmit

--- Scripts server/Cleanup-Profiles.ps1
+++ Scripts server/Cleanup-Profiles.ps1
@@ -1,12 +1,6 @@
-﻿Start-Transcript c:\temp\Cleanup-Profiles.log
-
-$UserProfiles = Get-CimInstance win32_userprofile
-Write-Host "=== GEVONDEN PROFIELEN ==="
+﻿$UserProfiles = Get-CimInstance win32_userprofile
 $UserProfiles | select LocalPath,LastUseTime | Sort LocalPath | Write-Host
 
 $OldUserProfiles = $UserProfiles | Where {$_.LastUseTime -lt $(Get-Date).Date.AddDays(-60) -and $_.LocalPath -notmatch 'admin' -and -not $_.Special}
-Write-Host "`n=== OP TE KUISEN PROFIELEN ==="
 $OldUserProfiles | select LocalPath,LastUseTime | Sort LocalPath | Write-Host
 $OldUserProfiles  | Remove-CimInstance
-
-Stop-Transcript
--- Scripts server/Create-LocalAdmin.ps1
+++ Scripts server/Create-LocalAdmin.ps1
@@ -1,29 +1,14 @@
-﻿Start-Transcript c:\temp\Create-LocalAdmin.log
-
-if ((Get-LocalUser administrator).Enabled) {
-    Write-Host "Disable built in administrator"
+﻿if ((Get-LocalUser administrator).Enabled) {
 	Disable-LocalUser Administrator
 }
 
 if (-not (Get-LocalUser -Name localadmin -ErrorAction Ignore)) {
-    Write-Host "Create localadmin"
 	$Password = ConvertTo-SecureString "%Jl<;CYN" -AsPlainText -Force
 	New-LocalUser localadmin -AccountNeverExpires -FullName "Lokale Admin" -Password $Password
 }
 
-if (-not (Get-LocalGroupMember administrators | ? Name -Match "localadmin")) { 
-    Write-Host "Local admin beheerder maken"
-    Add-LocalGroupMember -Group Administrators -Member localadmin
-}
+if (-not (Get-LocalGroupMember administrators | ? Name -Match "localadmin")) { Add-LocalGroupMember -Group Administrators -Member localadmin }
 
-if (-not (Get-LocalGroupMember administrators | ? Name -Match "localadmin")) { 
-    Write-Host "Local admin beheerder maken"
-    Add-LocalGroupMember -Group Administrators -Member localadmin
-}
+if (-not (Get-LocalGroupMember administrators | ? Name -Match "localadmin")) { Add-LocalGroupMember -Group Administrators -Member localadmin }
 
-if (Get-LocalUser orcadmin -ErrorAction SilentlyContinue) { 
-    Write-Host "Orcadmin scrappen" 
-    Remove-LocalUser orcadmin
-}
-
-Stop-Transcript
+if (Get-LocalUser orcadmin -ErrorAction SilentlyContinue) { Remove-LocalUser orcadmin }
