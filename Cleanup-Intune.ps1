e53dded97aef48af61df7506aa310c6cf854ed3d
Lieven Tronckoe <lieven.tronckoe@romerocollege.be>
2022-02-14 11:13:25 +0100

    .

--- Cleanup-Intune.ps1
+++ Cleanup-Intune.ps1
@@ -1,15 +1,9 @@
 ﻿$byod = Get-AzureADGroup -SearchString oud-BYOD-laptops | Get-AzureADGroupMember
-$byodDeviceId = $byod | select -ExpandProperty DeviceId
-$byodObjectId = $byod | select -ExpandProperty ObjectId
 $byodName = $byod | select -ExpandProperty DisplayName
 
 $alldevices = Get-AzureADGroup -SearchString intune-campus-all-devices | Get-AzureADGroupMember
-$allDeviceId = $alldevices | select -ExpandProperty DeviceId
-$allObjectId = $alldevices | select -ExpandProperty ObjectId
 $allName = $alldevices | select -ExpandProperty DisplayName
 
-$joinDeviceId = $allDeviceId + $byodDeviceId
-$joinObjectId = $allObjectId + $byodObjectId
 $joinName = $allName + $byodName
 
 
@@ -21,7 +15,3 @@ $data.value | ? deviceName -notin $joinName | % { Write-Host $_.deviceName; Remo
 
 $data = Get-IntuneManagedDevice
 $data.value | ? deviceName -notin $joinName | % { Write-Host $_.deviceName; Remove-IntuneManagedDevice -managedDeviceId $_.id }
-
-$fullList = Get-AzureADDevice -All $true
-
-$intunedevices = $data | select -ExpandProperty devicename
---
+++ Scripts Intune/Rename-EndpointDevices.ps1
@@ -0,0 +1,101 @@
+﻿Param
+(
+    [Parameter(Position=1,ValueFromPipeline=$true)]
+    [ValidateScript({Test-Path $_})] 
+    [string]$InputFile
+)
+
+if (-not (Get-InstalledModule Microsoft.Graph.Intune)) {
+    Install-PackageProvider -Name "NuGet" -Force
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    Install-Module Microsoft.Graph.Intune
+}
+
+if (-not (Get-InstalledModule windowsautopilotintune)) {
+    Install-PackageProvider -Name "NuGet" -Force
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    Install-Module windowsautopilotintune
+}
+
+$FileList = if ($input) {
+    $input | select -ExpandProperty FullName
+} elseif ($InputFile) {
+    $InputFile
+} else {
+    throw "No data found"
+}
+
+Write-Host "`n==========`nFETCH DATA`n==========`n"
+Connect-MSGraph
+$Devices = Get-IntuneManagedDevice | Get-MSGraphAllPages
+$AutopilotDevices = Get-AutopilotDevice
+
+foreach ($i in $FileList) {
+    $InputFile = $i
+    $InputFile -match "SP.*S\d" | Out-Null
+    $Leveringsnummer = $Matches[0]
+    
+    $Msg = "LEVERINGSNUMMER: $Leveringsnummer"
+    $Delimiter = "=" * $Msg.Length
+    Write-Host $Delimiter
+    Write-Host $Msg
+    Write-Host $Delimiter
+
+    Write-Host "`nENDPOINT`n--------"
+    Import-Excel -Path $InputFile | % {
+        $SignpostSerialNumber = $_.'Serienummer van apparaat'
+        $SignpostHostname = $_.Volgnummer
+        $Device = $Devices | ? serialNumber -eq $SignpostSerialNumber
+        if ($Device.deviceName -eq $SignpostHostname) {
+            return
+        }
+
+        Write-Host "Toestel $SignpostHostname met serienummer $SignpostSerialNumber"
+        if (-not $Device) {
+            Write-Host "    Toestel niet gevonden"
+            return
+        }
+        if ($Device.count -gt 1) {
+            Write-Host "    Meerdere toestellen gevonden"
+            return
+        }
+        Write-Host "    $($Device.deviceName) -> $SignpostHostname"
+        $DeviceID = $Device.id
+        $Resource = "deviceManagement/managedDevices('$DeviceID')/setDeviceName"
+        $GraphApiVersion = "Beta"
+        $URI = "https://graph.microsoft.com/$graphApiVersion/$($resource)"
+        $JSONPayload = "{ deviceName:`"$SignpostHostname`" }"
+        Invoke-MSGraphRequest -HttpMethod POST -Url $uri -Content $JSONPayload
+    }
+
+    Write-Host "`nAUTOPILOT`n---------"
+    Import-Excel -Path $InputFile | % {
+        $SignpostSerialNumber = $_.'Serienummer van apparaat'
+        $SignpostHostname = $_.Volgnummer
+    
+        $Device = $AutopilotDevices | ? serialNumber -eq $SignpostSerialNumber 
+        if (($Device.displayName -eq $SignpostHostname) -and ($Device.groupTag -eq $Leveringsnummer)) {
+            return
+        }
+
+        Write-Host "Toestel $SignpostHostname met serienummer $SignpostSerialNumber"
+        if (-not $Device) {
+            Write-Host "    Toestel niet gevonden"
+            return
+        }
+        if ($Device.count -gt 1) {
+            Write-Host "    Meerdere toestellen gevonden"
+            return
+        }
+
+        Write-Host "    $($Device.displayName) -> $SignpostHostname"
+        Write-Host "    $($Device.groupTag) -> $Leveringsnummer"
+        $DeviceID = $Device.id
+    
+        $graphApiVersion = "beta"
+        $Resource = "deviceManagement/windowsAutopilotDeviceIdentities"
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$Resource/$DeviceID/UpdateDeviceProperties"
+        $JSONPayload = "{ displayName:`"$SignpostHostname`", groupTag: `"$Leveringsnummer`" }"
+        Invoke-MSGraphRequest -HttpMethod POST -Url $uri -Content $JSONPayload
+    }
+}
