26c67c78eadc29461cd38e6f0cc24087ab02b7b8
Lieven Tronckoe <lieven.tronckoe@romerocollege.be>
2022-03-10 12:03:04 +0100

    Opmerkingen

--- Get-UsersAndDevices.ps1
+++ Get-UsersAndDevices.ps1
@@ -1,6 +1,7 @@
 $Users = Get-AzureADUser -Filter "Jobtitle eq 'leerling' and startswith(Department,'romerocollege_2')" -All $true
 Write-Host "Oplijsten info voor $($Users.Count) gebruikers..."
 $i = 0
+# TODO: batching
 $Users | % {
     Write-Progress -Activity "Toestellen zoeken..." -Status "$i/$($Users.Count) gedaan" -PercentComplete ($i / $Users.Count * 100)
     $i++
