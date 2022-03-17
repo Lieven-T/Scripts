158867147369c693aca732925825fd4c3386821d
Lieven Tronckoe <lieven.tronckoe@romerocollege.be>
2022-03-17 13:19:18 +0100

    Opkuis oude accounts is klaar

--- Opkuis-OudeAccounts.ps1
+++ Opkuis-OudeAccounts.ps1
@@ -1,5 +1,8 @@
 ﻿$YearCode = "2122_"
 Connect-Graph -Scopes @("Group.ReadWrite.All")
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes"
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No"
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
 
 ################################
 ### OPKUIS LEGE KLASSEN O365 ###
@@ -40,11 +43,16 @@ $Groups | % {
         Write-Error "Ongeldige klas met leerlingen: $($_.DisplayName)"
         return
     }
+    Write-Host "Opkuisen klas $($_.DisplayName)"
     $GroupsToRemove += [PSCustomObject][Ordered]@{
         Id=$_.DisplayName
         Method='DELETE'
         Url="/groups/$($_.ID)"
     }
+}
+$choiceRTN = $host.UI.PromptForChoice("OPKUISEN KLASSEN", "Opkuisen van $($GroupsToRemove.Count) klassen", $options, 1)
+if ( $choiceRTN -eq 1 ) {
+    return
 }
 
 for($i=0;$i -lt $GroupsToRemove.count;$i+=20) {
@@ -71,7 +79,7 @@ $Teams | % {
     }
 }
 for($i=0;$i -lt $TeamsToRemove.count;$i+=20){                                                                                                                                              
-    Write-Progress -Activity "Ongeldige teams verwijderen..." -Status "$i/$($TeamsToRemove.Count) ge#>#daan" -PercentComplete ($i / $TeamsToRemove.Count * 100)
+    Write-Progress -Activity "Ongeldige teams verwijderen..." -Status "$i/$($TeamsToRemove.Count) gedaan" -PercentComplete ($i / $TeamsToRemove.Count * 100)
     $Request = @{}                
     $Request['requests'] = ($TeamsToRemove[$i..($i+19)])
     $RequestBody = $Request | ConvertTo-Json -Depth 3
@@ -85,8 +93,7 @@ for($i=0;$i -lt $TeamsToRemove.count;$i+=20){
 # DEELTIJDS OPKUISEN #
 ######################
 
-$Groups = Get-MgGroup -Filter "startswith(displayName,'trefpunt')" | ? DisplayName -Match "\d"
-# ALLE GROEPLEDEN OPHALEN
+$Groups = Get-MgGroup -Filter "startswith(displayName,'trefpunt')" -All | ? DisplayName -Match "\d"
 $GroupMembers = @()
 for($i=0;$i -lt $Groups.count;$i+=20){                                                                                                                                              
     Write-Progress -Activity "Groepsleden zoeken..." -Status "$i/$($Groups.Count) gedaan" -PercentComplete ($i / $Groups.Count * 100)
@@ -111,11 +118,16 @@ $Groups | % {
     if (($GroupMembers | ? id -eq $_.DisplayName).body.value.count) {
         return
     }
+    Write-Host "Opkuisen klas $($_.DisplayName)"
     $GroupsToRemove += [PSCustomObject][Ordered]@{
         Id=$_.DisplayName
         Method='DELETE'
         Url="/groups/$($_.ID)"
     }
+}
+$choiceRTN = $host.UI.PromptForChoice("OPKUISEN KLASSEN", "Opkuisen van $($GroupsToRemove.Count) klassen", $options, 1)
+if ( $choiceRTN -eq 1 ) {
+    return
 }
 for($i=0;$i -lt $GroupsToRemove.count;$i+=20) {
     Write-Progress -Activity "Lege CLW-klassen verwijderen..." -Status "$i/$($GroupsToRemove.Count) gedaan" -PercentComplete ($i / $GroupsToRemove.Count * 100)
@@ -127,8 +139,6 @@ for($i=0;$i -lt $GroupsToRemove.count;$i+=20) {
         Write-Error "Probleem met $($_.id): $($_.body.error.message)"
     }
 }
-
-
 
 #####################################
 ### OPKUIS ONBESTAANDE KLASSEN AD ###
@@ -148,13 +158,15 @@ Get-ChildItem | ? Name -NE "inactief" | ? Name -NotIn $SmsClassList | % {
 }
 
 
-##########################
-### ONTERECHT IN EXTRA ###
-##########################
+################################
+### VERZAMELEN SPOOKACCOUNTS ###
+################################
 
-$Groups = Get-MgGroup -Filter "startswith(displayName,'hetlaar') or startswith(displayName,'basisromero') or startswith(displayName,'romerocollege') or startswith(displayName,'trefpunt')" -all
-#$Groups = $Groups | ? DisplayName -NotMatch "basisromero_\d[A-Z]|cvw_\d|romerocollege_\d|trefpunt_\w{2} \w{2}|_extra|_disabled|_leerlingen"
-# ALLE GROEPLEDEN OPHALEN: MOET ANDERS
+$SpookAccountID = "864f9916-6ebd-44ed-abc8-9629495142bc"
+
+# Zoek alle leden van adweaver-groepen die géén klassen zijn
+$AllGroups = Get-MgGroup -Filter "startswith(displayName,'hetlaar') or startswith(displayName,'basisromero') or startswith(displayName,'romerocollege') or startswith(displayName,'trefpunt')" -all
+$Groups = $AllGroups | ? DisplayName -NotMatch "basisromero_\d[A-Z]|cvw_\d|romerocollege_\d|trefpunt_\w{2} \w{2}"
 $GroupMembers = @()
 for($i=0;$i -lt $Groups.count;$i+=20){                                                                                                                                              
     Write-Progress -Activity "Groepsleden zoeken..." -Status "$i/$($Groups.Count) gedaan" -PercentComplete ($i / $Groups.Count * 100)
@@ -174,37 +186,47 @@ for($i=0;$i -lt $Groups.count;$i+=20){
     }
     $GroupMembers += $Response.responses
 }
-$RegularUserIDs = ($GroupMembers | ? Id -notmatch "_extra").body.value | select -Unique Id
 
-$ExtraMembersToRemove = @()
-$ExtraGroupId = ($Groups | ? DisplayName -Match "trefpunt_extra").Id
-($GroupMembers | ? Id -match "trefpunt_extra").body.value | select -Unique Id | ? { $_ -in $RegularUserIDs } | % {
-    $ExtraMembersToRemove += [PSCustomObject][Ordered]@{
-        Id=$_.Id
-        Method='DELETE'
-        Url="/groups/$ExtraGroupId/members/$($_.Id)/`$ref"
+$MemberList = $GroupMembers | % { 
+    [PSCustomObject][Ordered]@{
+        Id=$_.id
+        Members=$_.body.value
     }
 }
-$ExtraGroupId = ($Groups | ? DisplayName -Match "basisromero_extra").Id
-($GroupMembers | ? Id -match "basisromero_extra").body.value | select -Unique Id | ? { $_ -in $RegularUserIDs } | % {
-    $ExtraMembersToRemove += [PSCustomObject][Ordered]@{
-        Id=$_.Id
-        Method='DELETE'
-        Url="/groups/$ExtraGroupId/members/$($_.Id)/`$ref"
+$MemberList | ? { $_.Members.Count -ge 100 } | % {
+    Write-Host "Ophalen alle leden van groep $($_.Id)"
+    $_.Members = Get-MgGroupMember -GroupId ($Groups | ? DisplayName -eq $_.Id).Id -All
+}
+
+$SpookAccounts = Get-MgGroupMember -GroupId $SpookAccountID -All
+$RegularUserIDs = $MemberList | % { $_.Members | select -ExpandProperty ID } | Select -Unique
+$RegularAndGhostUserIDs = $RegularUserIDs + ($SpookAccounts | select -ExpandProperty ID) | select -Unique
+$AllUsers = Get-MgUser -all | ? UserPrincipalName -NotMatch "^package|#EXT#"
+
+# Alle gebruikers die niét in een ADWeaver-hoofdgroep of de spookgroep zitten, toevoegen aan de spookgroep
+$UsersToAdd = @()
+$AllUsers | ? Id -NotIn $RegularAndGhostUserIDs | % {
+    Write-Host "Toevoegen $($_.UserPrincipalName) aan Spookaccounts"
+    $Headers = [PSCustomObject][Ordered]@{"Content-Type"="application/json"}
+    $Body = [PSCustomObject][Ordered]@{ 
+        "@odata.id" = "https://graph.microsoft.com/v1.0/users/$($_.id)"
+    }
+    $UsersToAdd += [PSCustomObject][Ordered]@{
+        Id=$_.UserPrincipalName
+        Method='POST'
+        Url="groups/$SpookAccountID/members/`$ref"
+        Headers=$Headers
+        Body=$Body
     }
 }
-$ExtraGroupId = ($Groups | ? DisplayName -Match "romerocollege_extra").Id
-($GroupMembers | ? Id -match "romerocollege_extra").body.value | select -Unique Id | ? { $_ -in $RegularUserIDs } | % {
-    $ExtraMembersToRemove += [PSCustomObject][Ordered]@{
-        Id=$_.Id
-        Method='DELETE'
-        Url="/groups/$ExtraGroupId/members/$($_.Id)/`$ref"
-    }
+$choiceRTN = $host.UI.PromptForChoice("SPOOKACCOUNTS VERZAMELEN", "Verzamelen van $($UsersToAdd.Count) accounts", $options, 1)
+if ( $choiceRTN -eq 1 ) {
+    return
 }
-for($i=0;$i -lt $ExtraMembersToRemove.count;$i+=20) {
-    Write-Progress -Activity "Onterecht in extra opkuisen..." -Status "$i/$($ExtraMembersToRemove.Count) gedaan" -PercentComplete ($i / $ExtraMembersToRemove.Count * 100)
+for($i=0;$i -lt $UsersToAdd.count;$i+=20) {
+    Write-Progress -Activity "Accounts verzamelen..." -Status "$i/$($UsersToAdd.Count) gedaan" -PercentComplete ($i / $UsersToAdd.Count * 100)
     $Request = @{}           
-    $Request['requests'] = ($ExtraMembersToRemove[$i..($i+19)])
+    $Request['requests'] = ($UsersToAdd[$i..($i+19)])
     $RequestBody = $Request | ConvertTo-Json -Depth 4
     $Response = Invoke-GraphRequest -Uri 'https://graph.microsoft.com/beta/$batch' -Body $RequestBody -Method POST -ContentType "application/json"
     $Response.responses | ? status -ne "204" | % {
@@ -213,22 +235,60 @@ for($i=0;$i -lt $ExtraMembersToRemove.count;$i+=20) {
 }
 
 #######################################
-### OPKUIS ONBEHEERDE ACCOUNTS O365 ###
+### OPKUISEN EXTRA EN SPOOKACCOUNTS ###
 #######################################
 
-$AllUsers = Get-MgUser -all
-
-$OudeLeraars = (Get-AzureADGroup -SearchString oudeleraars).ObjectId
-$OudeLln = (Get-AzureADGroup -SearchString oudelln).ObjectId
-
-# Opkuis leraars
-$users | ? { $_.ObjectId -notin $allusers -and $_.Mail -match "@romerocollege.be" } | % {
-    Write-Host $_.UserPrincipalName
-    Add-AzureADGroupMember -ObjectId $OudeLeraars -RefObjectId $_.ObjectId
+# SPOOKGROEP UITKUISEN
+$MembersToRemove = @()
+$SpookAccounts | ? ID -in $RegularUserIDs  | % {
+    Write-Host "Schrappen $($_.AdditionalProperties.userPrincipalName) uit Spookaccounts"
+    $MembersToRemove += [PSCustomObject][Ordered]@{
+        Id=$_.AdditionalProperties.userPrincipalName
+        Method='DELETE'
+        Url="groups/$SpookAccountID/members/$($_.id)/`$ref"
+    }
+}
+$choiceRTN = $host.UI.PromptForChoice("SPOOKACCOUNTS SCHRAPPEN", "Schrappen van $($MembersToRemove.Count) accounts", $options, 1)
+if ( $choiceRTN -eq 1 ) {
+    return
+}
+for($i=0;$i -lt $MembersToRemove.count;$i+=20) {
+    Write-Progress -Activity "Accounts schrappen..." -Status "$i/$($MembersToRemove.Count) gedaan" -PercentComplete ($i / $MembersToRemove.Count * 100)
+    $Request = @{}           
+    $Request['requests'] = ($MembersToRemove[$i..($i+19)])
+    $RequestBody = $Request | ConvertTo-Json -Depth 4
+    $Response = Invoke-GraphRequest -Uri 'https://graph.microsoft.com/beta/$batch' -Body $RequestBody -Method POST -ContentType "application/json"
+    $Response.responses | ? status -ne "204" | % {
+        Write-Error "Probleem met $($_.id): $($_.body.error.message)"
+    }
 }
 
-# Opkuis lln
-$users | Where-Object { $_.ObjectId -notin $allusers -and $_.Mail -match "@student.romerocollege.be" } | % {
-    Write-Host $_.UserPrincipalName
-    Add-AzureADGroupMember -ObjectId $OudeLln -RefObjectId $_.ObjectId
+# EXTRA UITKUISEN
+$RegularUserWithoutExtraIDs = $MemberList | ? Id -NotMatch "_extra$" | % { $_.Members | select -ExpandProperty ID } | Select -Unique
+$MemberList | ? Id -match "_extra$" | % {
+    $CurrentGroupName = $_.Id
+    $CurrentGroupId = ($Groups | ? DisplayName -eq $CurrentGroupName).Id
+    $MembersToRemove = @()
+    $_.MemberList | ? ID -in $RegularUserWithoutExtraIDs | % {
+        Write-Host "Schrappen $($_.AdditionalProperties.userPrincipalName) uit $CurrentGroupName"
+        $MembersToRemove += [PSCustomObject][Ordered]@{
+            Id=$_.AdditionalProperties.userPrincipalName
+            Method='DELETE'
+            Url="groups/$CurrentGroupId/members/$($_.id)/`$ref"
+        }
+    }
+    $choiceRTN = $host.UI.PromptForChoice("EXTRA-ACCOUNTS SCHRAPPEN", "Schrappen van $($MembersToRemove.Count) accounts uit $CurrentGroupName", $options, 1)
+    if ( $choiceRTN -eq 1 ) {
+        return
+    }
+    for($i=0;$i -lt $MembersToRemove.count;$i+=20) {
+        Write-Progress -Activity "Accounts schrappen..." -Status "$i/$($MembersToRemove.Count) gedaan" -PercentComplete ($i / $MembersToRemove.Count * 100)
+        $Request = @{}           
+        $Request['requests'] = ($MembersToRemove[$i..($i+19)])
+        $RequestBody = $Request | ConvertTo-Json -Depth 4
+        $Response = Invoke-GraphRequest -Uri 'https://graph.microsoft.com/beta/$batch' -Body $RequestBody -Method POST -ContentType "application/json"
+        $Response.responses | ? status -ne "204" | % {
+            Write-Error "Probleem met $($_.id): $($_.body.error.message)"
+        }
+    }
 }
