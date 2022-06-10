38ffeb2a91f44f34f8ab09ff4b4ae3aebc3a6b18
Lieven Tronckoe <lieven.tronckoe@romerocollege.be>
2022-06-10 10:08:27 +0200

    Extra enrollment info

--- Get-UsersAndDevices.ps1
+++ Get-UsersAndDevices.ps1
@@ -1,5 +1,5 @@
 Connect-Graph -Scopes @("User.ReadBasic.All","User.Read.All","DeviceManagementManagedDevices.Read.All","Directory.Read.All")
-$Users = Get-MgUser -Filter "Jobtitle eq 'leerling' and startswith(Department,'romerocollege_2')" -All -Property @("Department","Id","UserPrincipalName","JobTitle")
+$Users = Get-MgUser -Filter "startswith(Department,'romerocollege_')" -All -Property @("Department","Id","UserPrincipalName","JobTitle") | ? Department -Match 'romerocollege_3'
 
 Write-Host "Oplijsten info voor $($Users.Count) gebruikers..."
 $Output = for($i=0;$i -lt $Users.count;$i+=20){                                                                                                                                              
@@ -19,13 +19,43 @@ $Output = for($i=0;$i -lt $Users.count;$i+=20){
     }
     $Users[$i..($i+19)] | % {
         $Device = ($Response.responses | ? id -eq $_.UserPrincipalName).body.value
-        [PSCustomObject][Ordered]@{
-            Gebruiker=$_.UserPrincipalName
-            Klas=($_.Department -replace 'romerocollege.*_','')
-            Rol=$_.JobTitle
-            Toestel=$Device.deviceName
-            'Toestel ID'=$Device.id
+        if ($Device -is [Array]) {
+                $Rol = $_.JobTitle
+                $Gebruiker = $_.UserPrincipalName
+                $Klas = ($_.Department -replace 'romerocollege.*_','')
+                if (-not $Device.Count) {
+                    [PSCustomObject][Ordered]@{
+                        Gebruiker = $Gebruiker
+                        Klas = $Klas
+                        Rol = $Rol
+                        Toestel = ''
+                        'Toestel ID'= ''
+                        Model = ''
+                        EnrolledDateTime = ''
+                        LastSyncDateTime = ''
+                    }
+                }
+                $Device | % {
+                    [PSCustomObject][Ordered]@{
+                        Gebruiker = $Gebruiker
+                        Klas = $Klas
+                        Rol = $Rol
+                        Toestel = $_.deviceName
+                        'Toestel ID'= $_.Id
+                        Model = $_.Model
+                        EnrolledDateTime = $_.enrolledDateTime
+                        LastSyncDateTime = $_.lastSyncDateTime
+                    }
+                }
+        } else {
+            [PSCustomObject][Ordered]@{
+                Gebruiker=$_.UserPrincipalName
+                Klas=($_.Department -replace 'romerocollege.*_','')
+                Rol=$_.JobTitle
+                Toestel=$Device.deviceName
+                'Toestel ID'=$Device.id
+            }
         }
     }
 }
-$Output | Export-Excel c:\temp\toestelinfo.xlsx
+$Output | Sort -Property Klas,Gebruiker | Export-Excel c:\temp\toestelinfo.xlsx
